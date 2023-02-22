1ce13e242537412abc66bbac24d4423d1cbc6cc5
Berk Disli <berkdisli@yahoo.com>
2023-02-22 18:42:06 +0100

    22.02 character, list and home

--- src/Components/CharactersList.js
+++ src/Components/CharactersList.js
@@ -1,6 +1,16 @@
-const CharactersList = () => {
-    // to return the character components
-    return ( <></> );
-}
- 
+import React from 'react';
+
+import Character from './Character';
+
+const CharactersList = (props) => {
+    const { data } = props;
+    return (
+        <section className="data">
+            {data.map((data) => (
+                <Character key={data.id} data={data} />
+            ))}
+        </section>
+    );
+};
+
 export default CharactersList;
--- src/Components/Home.js
+++ src/Components/Home.js
@@ -1,6 +1,41 @@
+import React, { useState, useEffect } from "react";
+
+import CharactersList from './CharactersList';
+const API_URL = 'https://rickandmortyapi.com/api/character';
+
 const Home = () => {
-    // gonna display here CharactersList
-    return ( <></> );
+
+    const [data, setData] = useState([]);
+    const [error, setError] = useState(null);
+    const [isPending, setIsPending] = useState(false);
+
+    useEffect(() => {
+        setIsPending(true);
+        fetch(API_URL)
+            .then((res) => {
+                if (!res.ok) {
+                    throw new Error('Fetching failed');
+                }
+                return res.json();
+            })
+            .then((json) => {
+                setData(json);
+                setIsPending(false);
+                setError(null);
+            })
+            .catch((e) => {
+                setData([]);
+                setIsPending(false);
+                setError(e.message);
+            });
+    }, []);
+    return (
+        <div className="home">
+            {error && <div>{error}</div>}
+            {isPending && <div>Loading...</div>}
+            {data && <CharactersList data={data} title="All Characters" />}
+        </div>
+    );
 }
- 
+
 export default Home;
