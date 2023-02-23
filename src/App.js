8e8ba4756ea7717c31944443600e3d04a46ed270
AnyaKononchenko <anya.kononchenko@gmail.com>
2023-02-23 19:13:35 +0100

    added go to character details

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import './App.css';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import {useNavigate, BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Header from "./components/Header";
 import Footer from "./components/Footer";
 import Home from './components/Home';
@@ -8,13 +8,17 @@ import { NotFound } from './components/handlers';
 import useFetch from './services/useFetch'
 import { useState, useEffect } from 'react';
 import { Error, Loading } from './components/handlers';
+import CharacterDetails from './components/CharacterDetails';
 
 
 function App() {
-
   const { data, error, isPending } = useFetch();
 
   const [characters, setCharacters] = useState([]);
+
+  useEffect(() => {
+    setCharacters(data && data.results);
+  }, [data]);
 
   const onCreate = (character) => {
     const newCharacter = { id: characters.length + 1, ...character };
@@ -22,10 +26,6 @@ function App() {
       return [...prevState, newCharacter];
     });
   }
-
-  useEffect(() => {
-    setCharacters(data && data.results);
-  }, [data])
 
   return (
     <div className="page-content">
@@ -38,6 +38,7 @@ function App() {
             <Routes>
               <Route exact path='/' element={<Home data={characters} />} />
               <Route path='/create' element={<NewCharacter onCreate={onCreate} />} />
+              <Route path='characters/:id' element={<CharacterDetails/>} />
               <Route path='*' element={<NotFound />} />
             </Routes>
           </main>}
