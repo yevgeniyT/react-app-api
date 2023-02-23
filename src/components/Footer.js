f7391f580b8fd726f4426d6bcaffe0934d4eed1c
yevgeniyT <yevgentolkachov@gmail.com>
2023-02-23 22:01:42 +0100

    Add footer structure

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,7 +1,45 @@
-const Footer = () => {
-    return ( 
-        <footer>Footer</footer>
-     );
-}
+// const Footer = () => {
+//     return ( 
+//         <footer>Footer</footer>
+//      );
+// }
  
+// export default Footer;
+
+import React from 'react';
+
+import {FaYoutube} from 'react-icons/fa';
+import {FaInstagram} from 'react-icons/fa';
+
+const Footer = () => {
+  const creatorLinks = [
+    {
+      name: 'Anna Kononchenko',
+      link: 'https://github.com/AnyaKononchenko',
+    },
+    {
+      name: 'Berk Disli',
+      link: 'https://github.com/berkdisli',
+    },
+    {
+      name: 'Yevgeniy Tolkachev',
+      link: 'https://github.com/yevgeniyT',
+    },
+  ];
+
+  return (
+    <footer>
+      <div>
+        <a href="https://www.youtube.com/" target="_blank" rel="noopener noreferrer"> <FaYoutube/></a>
+        <a href="https://www.instagram.com/" target="_blank" rel="noopener noreferrer"><FaInstagram/></a>
+      </div>
+      <div>
+        Created by: {creatorLinks.map((creator) => (
+          <a href={creator.link} key={creator.name} target="_blank" rel="noopener noreferrer">{creator.name}</a>
+        ))}
+      </div>
+    </footer>
+  );
+}
+
 export default Footer;
