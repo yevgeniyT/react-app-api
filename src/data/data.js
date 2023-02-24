e01b0241af755ae2eaea7e89454baf5ed85784f9
AnyaKononchenko <anya.kononchenko@gmail.com>
2023-02-24 19:55:44 +0100

    improved form

---
+++ src/data/data.js
@@ -0,0 +1,87 @@
+
+export const images = [
+    {
+        id: 1,
+        name: "young man",
+        src: "https://cdn-icons-png.flaticon.com/512/9137/9137821.png"
+    },
+    {
+        id: 2,
+        name: "doctor",
+        src: "https://cdn-icons-png.flaticon.com/512/5798/5798254.png"
+    },
+    {
+        id: 3,
+        name: "stylish man",
+        src: "https://cdn-icons-png.flaticon.com/512/5300/5300700.png"
+    },
+    {
+        id: 4,
+        name: "masked man",
+        src: "https://cdn-icons-png.flaticon.com/512/9565/9565148.png"
+    },
+    {
+        id: 5,
+        name: "young woman",
+        src: "https://cdn-icons-png.flaticon.com/512/9137/9137825.png"
+    },
+    {
+        id: 6,
+        name: "young woman in hat",
+        src: "https://cdn-icons-png.flaticon.com/512/9420/9420516.png"
+    },
+    {
+        id: 7,
+        name: "young man in hat",
+        src: "https://cdn-icons-png.flaticon.com/512/9420/9420513.png"
+    },
+    {
+        id: 8,
+        name: "man",
+        src: "https://cdn-icons-png.flaticon.com/512/5295/5295039.png"
+    },
+    {
+        id: 9,
+        name: "old man",
+        src: "https://cdn-icons-png.flaticon.com/512/9434/9434784.png"
+    },
+    {
+        id: 10,
+        name: "old woman",
+        src: "https://cdn-icons-png.flaticon.com/512/9267/9267562.png"
+    },
+    {
+        id: 11,
+        name: "angry alien",
+        src: "https://cdn-icons-png.flaticon.com/512/3387/3387148.png"
+    },
+    {
+        id: 12,
+        name: "classic alien",
+        src: "https://cdn-icons-png.flaticon.com/512/3204/3204784.png"
+    },
+    {
+        id: 13,
+        name: "monster alien",
+        src: "https://cdn-icons-png.flaticon.com/512/2463/2463666.png"
+    },
+    {
+        id: 14,
+        name: "kinda pretty alien",
+        src: "https://cdn-icons-png.flaticon.com/512/2463/2463677.png"
+    },
+    {
+        id: 15,
+        name: "chilling alien",
+        src: "https://cdn-icons-png.flaticon.com/512/3506/3506082.png"
+    },
+    {
+        id: 16,
+        name: "techno alien",
+        src: "https://cdn-icons-png.flaticon.com/512/6031/6031654.png"
+    },
+]
+
+export const status = [{ id: 1, name: 'alive' }, { id: 2, name: 'dead' }, { id: 3, name: 'unknown' }];
+
+export const species = [{ id: 1, name: 'human' }, { id: 2, name: 'alien' }, { id: 3, name: 'unknown' }];
