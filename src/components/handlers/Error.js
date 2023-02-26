5d09d0afce841c91fa19e219962342c5cf4b93ac
AnyaKononchenko <anya.kononchenko@gmail.com>
2023-02-26 01:34:07 +0100

    updated handlers

--- src/components/handlers/Error.js
+++ src/components/handlers/Error.js
@@ -1,9 +1,13 @@
-const Error = ({message}) => {
-    return ( 
-        <section className="error">
+import logo from '../../images/error-animated.gif';
+
+
+const Error = ({ message }) => {
+    return (
+        <section className="error flex-centered">
+            <img className="error__logo" src={logo} alt="error logo" />
             <p className="error__body">{message}</p>
         </section>
-     );
+    );
 }
- 
+
 export default Error;
