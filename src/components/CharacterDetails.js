3d005760ce565db6b6167abbeee5212c1fd630ba
AnyaKononchenko <anya.kononchenko@gmail.com>
2023-02-26 11:36:33 +0100

    changed <a> to <p> in details

--- src/components/CharacterDetails.js
+++ src/components/CharacterDetails.js
@@ -1,19 +1,20 @@
-import {useParams } from "react-router-dom";
+import {useParams} from "react-router-dom";
+
 
 const CharacterDetails = ({data}) => {
     const { id } = useParams();
     const index = data.findIndex(character => character.id === parseInt(id));
     return (
         <section className="details flex-centered">
-         <div className="character-ditails">
+         <div className="character-details">
             <div>
                 <h4 className="ditails__header">{data[index].name}</h4>
             </div>            
-            <div className="conteiner__col character">
+            <div className="container__col character">
                 <img className="character__img" src={data[index].image} alt="Rick and Morty" />
                     <div className='character__dsc'>
                         <div className="col-75 type">
-                            {["Ditaild information:",
+                            {["Detailed information:",
                             `createrd at: ${data[index].created}`,
                             `Status: ${data[index].status}`,
                             `Species: ${data[index].species}`,
@@ -22,14 +23,14 @@ const CharacterDetails = ({data}) => {
                             <p className="typed" style={{animationDelay: `${index * 1}s`}}>{text}</p> ))}
                         </div>
 
-                        <div className="conteiner__col col-25" >
+                        <div className="container__col col-25" >
                             <div>
                                 <p>Origin:</p>
-                                <a className="character__link"  href={data[index].origin.url}>{data[index].origin.name}</a>
+                                <p className="character__link">{data[index].origin.name}</p>
                             </div>
                             <div>
                                 <p>Current location:</p>
-                                <a className="character__link" href={data[index].location.url}>{data[index].location.name}</a>
+                                <p className="character__link">{data[index].location.name}</p>
                             </div>
                         </div>
                     </div>
